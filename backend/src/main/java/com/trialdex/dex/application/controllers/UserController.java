6128fa815f32e958f55e7880c59e7e3447b2c849
leonab <nabeelkhawar95@yahoo.com>
2022-05-24 01:46:17 +0530

    add gradle plugin to run npm commands; copy resources to webjar; add controller to forward spring to client routes

--- backend/src/main/java/com/trialdex/dex/application/controllers/UserController.java
+++ backend/src/main/java/com/trialdex/dex/application/controllers/UserController.java
@@ -4,13 +4,12 @@ import com.trialdex.dex.models.User;
 import com.trialdex.dex.services.IUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -18,6 +17,7 @@ import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class UserController {
 
     private final IUserService userService;
